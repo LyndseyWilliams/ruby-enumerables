5141a72a88a7031374c4321c6748f49cd1953203
Lyndsey Williams <lywilliams@gmail.com>
2014-06-23 13:30:15 -0700

    add reduce and each slice

--- enumerables_review.rb
+++ enumerables_review.rb
@@ -11,6 +11,28 @@ def print_odd_indexed_integers(array)
 end
 
 
+# TODO: Take an Array of Strings and return a new Array with an exclamation point appended to each String.
+#       You should make use of Enumerable#map
+def add_bang(array)
+	array.map {|string| string + "!"}
+end
+
+
+# TODO: Calculate the sum of an Array of numbers.
+#       You should make use of Enumerable#reduce
+def sum(array)
+	array.reduce(:+)
+end
+
+# TODO: Reorganize an Array of the elements into groups of 3, and then sort each group alphabetically.
+#       You should make use of Enumerable#each_slice
+def sorted_triples(array)
+	new_array = []
+	array.each_slice(3) {|grouped_words| new_array << grouped_words.sort}
+	new_array
+end
+
+
 
 # Driver code... don't change this.
 puts print_odd_indexed_integers([2, 4, 6, 8, 10, 12]) # this should print "4\n8\n12"
